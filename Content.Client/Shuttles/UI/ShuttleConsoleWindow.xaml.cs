4ec4870f0455666eb281cf644f37144436ff011c
TetrisZz <TetrisZz_os@mail.ru>
2025-06-04 22:07:21 +1000

    Revert "FTL Carriers"

--- Content.Client/Shuttles/UI/ShuttleConsoleWindow.xaml.cs
+++ Content.Client/Shuttles/UI/ShuttleConsoleWindow.xaml.cs
@@ -22,8 +22,6 @@ public sealed partial class ShuttleConsoleWindow : FancyWindow,
 
     public event Action<NetEntity, NetEntity>? DockRequest;
     public event Action<NetEntity>? UndockRequest;
-    public event Action<List<NetEntity>>? UndockAllRequest;
-    public event Action<List<NetEntity>, bool>? ToggleFTLLockRequest;
 
     public ShuttleConsoleWindow()
     {
@@ -63,16 +61,6 @@ public sealed partial class ShuttleConsoleWindow : FancyWindow,
         DockContainer.UndockRequest += entity =>
         {
             UndockRequest?.Invoke(entity);
-        };
-        
-        DockContainer.UndockAllRequest += dockEntities =>
-        {
-            UndockAllRequest?.Invoke(dockEntities);
-        };
-        
-        DockContainer.ToggleFTLLockRequest += (dockEntities, enabled) =>
-        {
-            ToggleFTLLockRequest?.Invoke(dockEntities, enabled);
         };
 
         NfInitialize(); // Frontier Initialization for the ShuttleConsoleWindow
